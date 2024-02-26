a7464423ffaad141e3a5465675e1adc1eaa698a8
BakuEmilov <emilovbaku530@gmail.com>
2024-02-26 22:00:46 +0600

    the first api

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+
+import { useSearchUsersQuery } from "@/store/features/github/githubApi";
 import Image from "next/image";
 
 export default function Home() {
+
+ const {isLoading, isError, data} = useSearchUsersQuery("vladilen");
+
   return (
     <main>
       <h1>Hello World</h1>
